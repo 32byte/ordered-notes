a228939c8878f9f12342844c7c723d5302ce397d
byteco <xlebedenko@gmail.com>
2021-06-16 21:05:20 +0200

    Funcionallity done

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace OrderedNotes
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.inSens = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTopmost = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.inSens)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTopmost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 12);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(89, 23);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "New Note";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(107, 12);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(91, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // inSens
+            // 
+            this.inSens.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.inSens.Location = new System.Drawing.Point(78, 55);
+            this.inSens.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.inSens.Name = "inSens";
+            this.inSens.Size = new System.Drawing.Size(149, 20);
+            this.inSens.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sensitivity:";
+            // 
+            // btnTopmost
+            // 
+            this.btnTopmost.Image = global::OrderedNotes.Properties.Resources.pin_on;
+            this.btnTopmost.Location = new System.Drawing.Point(204, 12);
+            this.btnTopmost.Name = "btnTopmost";
+            this.btnTopmost.Size = new System.Drawing.Size(23, 23);
+            this.btnTopmost.TabIndex = 4;
+            this.btnTopmost.TabStop = false;
+            this.btnTopmost.Click += new System.EventHandler(this.btnTopmost_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 87);
+            this.Controls.Add(this.btnTopmost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inSens);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnNew);
+            this.Name = "MainForm";
+            this.Text = "Ordered Notes";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.inSens)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTopmost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.NumericUpDown inSens;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox btnTopmost;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OrderedNotes.Properties;
+using System.Collections.Generic;
+
+namespace OrderedNotes
+{
+    public partial class MainForm : Form
+    {
+        private List<Note> notes = new List<Note>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            new ToolTip().SetToolTip(this.btnTopmost, "Toggle topmost property of this window");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.Location = Settings.Default.MainPos;
+            this.TopMost = Settings.Default.TopMost;
+            this.inSens.Value = Settings.Default.Sensitivity;
+
+            if (this.TopMost)
+                this.btnTopmost.Image = Resources.pin_on;
+            else
+                this.btnTopmost.Image = Resources.pin_off;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.MainPos = this.Location;
+            Settings.Default.TopMost = this.TopMost;
+            Settings.Default.Sensitivity = (int) this.inSens.Value;
+
+            Settings.Default.Save();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            Note note = new Note(notes.Count + 1);
+
+            note.Visible = true;
+
+            notes.Add(note);
+            this.Focus();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            notes.RemoveAll(s => s.Visible == false);
+
+            if (notes.Count == 0)
+                return;
+
+            notes.Sort(delegate (Note a, Note b) {
+                var aCoords = a.getCoords();
+                var bCoords = b.getCoords();
+
+                var playRoom = this.inSens.Value;
+
+                var yDiff = bCoords[0] - aCoords[0];
+                var xDiff = bCoords[1] - aCoords[1];
+
+                if (yDiff > playRoom)
+                    return -1; // prefer first
+                else if (yDiff < -playRoom)
+                    return 1; // prefer second
+                else if (xDiff > playRoom)
+                    return -1; // prefer first
+                else
+                    return 1; // prefer other
+            });
+
+            string result = "";
+
+            foreach(Note note in notes)
+            {
+                result += note.getText() + "\n\n";
+                Console.WriteLine(note.Text + ": " + note.getCoords()[0] + " " + note.getCoords()[1]);
+            }
+
+            result = result.Trim();
+
+            new Note(result).Show();
+        }
+
+        private void btnTopmost_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+
+            if (this.TopMost)
+                this.btnTopmost.Image = Resources.pin_on;
+            else
+                this.btnTopmost.Image = Resources.pin_off;
+        }
+    }
+
+    public class Note : Form
+    {
+        private RichTextBox tbNote = new RichTextBox();
+
+        public Note(int number)
+        {
+            this.Text = "Note #" + number;
+
+            this.tbNote.Top = 0;
+            this.tbNote.Left = 0;
+            this.tbNote.Width = this.Width - 13;
+            this.tbNote.Height = this.Height - 38;
+            this.tbNote.Font = new Font("Arial", 18, FontStyle.Regular);
+
+            this.Controls.Add(tbNote);
+            this.Resize += OnResizeEvent;
+        }
+
+        public Note(string result)
+        {
+            this.Text = "Export";
+
+            this.tbNote.Top = 0;
+            this.tbNote.Left = 0;
+            this.tbNote.Width = this.Width - 13;
+            this.tbNote.Height = this.Height - 38;
+            this.tbNote.Font = new Font("Arial", 18, FontStyle.Regular);
+
+            this.Controls.Add(tbNote);
+            this.Resize += OnResizeEvent;
+
+            this.tbNote.Text = result;
+            this.tbNote.ReadOnly = true;
+        }
+
+        public void OnResizeEvent(object sender, EventArgs e)
+        {
+            this.tbNote.Width = this.Width - 13;
+            this.tbNote.Height = this.Height - 38;
+        }
+
+        public string getText()
+        {
+            return this.tbNote.Text;
+        }
+
+        public int[] getCoords()
+        {
+            int[] coords = { this.Top, this.Left };
+
+            return coords;
+        }
+    }
+}
